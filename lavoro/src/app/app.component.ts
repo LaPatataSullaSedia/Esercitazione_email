ca108d9e5713400219ea14ae78a3ce21c146f781
wheelchaired_potato <marro.simone@iisgalvanimi.edu.it>
2025-04-23 17:42:57 +0000

    secondo

--- lavoro/src/app/app.component.ts
+++ lavoro/src/app/app.component.ts
@@ -10,4 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'lavoro';
+
+  piu(numero : HTMLInputElement) : boolean
+  {
+
+    return false;
+  }
+
+  meno(numero : HTMLInputElement) : boolean
+  {
+
+    return false;
+  }
 }
